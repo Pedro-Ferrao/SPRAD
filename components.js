843e7fc0e53f7469e60ee2ec594a604b05be1b5d
Pedro Ferrão <ferraopedro10@gmail.com>
2026-07-01 13:07:18 -0300

    correção slider & link do linkedin

--- components.js
+++ components.js
@@ -81,7 +81,7 @@ const footerHTML = `
                         <line x1="17.5" y1="6.5" x2="17.51" y2="6.5"/>
                     </svg>
                 </a>
-                <a href="https://www.linkedin.com/company/sp-rad-servi%C3%A7os-em-prote%C3%A7%C3%A3o-radiol%C3%B3gica/?originalSubdomain=br" target="_blank" aria-label="LinkedIn">
+                <a href="https://br.linkedin.com/company/sprad-protecaoradiologica" target="_blank" aria-label="LinkedIn">
                     <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
                         <path d="M16 8a6 6 0 0 1 6 6v7h-4v-7a2 2 0 0 0-2-2 2 2 0 0 0-2 2v7h-4v-7a6 6 0 0 1 6-6z"/><rect x="2" y="9" width="4" height="12"/><circle cx="4" cy="4" r="2"/>
                     </svg>
